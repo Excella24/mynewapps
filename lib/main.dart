b25d62c2c80ac82a7dbf875764bb82494ed40f03
Putra Dwiyanto <putra.dwiyanto@photoninfotech.net>
2020-02-07 16:14:06 +0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mynewapp/services/authentication.dart';
+import 'package:mynewapp/models/rootPage.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'My New App',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
---
+++ lib/screens/splash.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'package:splashscreen/splashscreen.dart';
+// import 'package:mynewapp/screens/login.dart';
+// import 'package:mynewapp/main.dart';
+
+// class splashScreen extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new SplashScreen(
+//       seconds: 5,
+//       navigateAfterSeconds: new LoginScreen(),
+//       title: new Text('Welcome In XXX App',
+//       style: new TextStyle(
+//         fontWeight: FontWeight.bold,
+//         fontSize: 20.0
+//       ),),
+//       image: Image.asset("assets/logo-splash.png"),
+//       backgroundColor: Colors.white,
+//       styleTextUnderTheLoader: new TextStyle(),
+//       photoSize: 100.0,
+//       loaderColor: Colors.red
+//     );
+//   }
+// }
